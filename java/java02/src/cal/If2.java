460d465b7c1c00e709029c26e645a6382713afe9
ckhoovv <49705386+ckhoovv@users.noreply.github.com>
2019-04-17 12:49:20 +0900

    Add files via upload

---
+++ java/java02/src/cal/If2.java
@@ -0,0 +1,19 @@
+package cal;
+
+public class If2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int x = 400;
+		int y = 400 % 2;
+		
+		if(y % 2 == 0) {
+			System.out.println("짝수입니다.");
+		}else {
+			System.out.println("홀수입니다.");
+		}
+
+	}
+
+}
