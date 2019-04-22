3719ac70f16dd1a0c8b08725b11c3904780edb95
ckhoovv <49705386+ckhoovv@users.noreply.github.com>
2019-04-22 15:09:23 +0900

    Add files via upload

---
+++ src/반복문/Test3.java
@@ -0,0 +1,25 @@
+package ¹Ýº¹¹®;
+
+
+public class Test3 {
+
+	public static void main(String[] args) {
+		
+		int num1 = 1;
+		int num2 = 20;
+		
+		while(num1 <= num2) {
+			
+			
+			num1 = num1 + 1;
+			System.out.println("Hello Java!");
+			
+		}
+		
+		
+		
+		
+		
+	}
+
+}
